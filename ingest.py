562029d3f18b4a5e30d0f3a08cca2ee2c519b705
zephyr2403 <ashutosh.extrimis.2403@gmail.com>
2024-09-21 06:29:28 +0000

    ingest.py: add download_and_save dataset

---
+++ ingest.py
@@ -0,0 +1,19 @@
+#ingest.py
+import os
+import pandas as pd 
+from datasets import load_dataset
+
+def download_and_save(folder_path='./dataset'):
+
+    dataset = load_dataset('ImperialCollegeLondon/health_fact',trust_remote_code=True)
+    
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+
+    train = pd.DataFrame(dataset['train'])
+    test = pd.DataFrame(dataset['test'])
+    valiadate = pd.DataFrame(dataset['validation'])
+    
+    train.to_csv(os.path.join(folder_path, 'train.csv'), index=False)
+    test.to_csv(os.path.join(folder_path, 'test.csv'), index=False)
+    valiadate.to_csv(os.path.join(folder_path, 'validation.csv'), index=False)
